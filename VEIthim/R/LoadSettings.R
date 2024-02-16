e9f1855eb681ea0b304b480126b9aaf192b01238
Theodore Mansfield <theodore.mansfield@rsginc.com>
2024-02-15 20:25:06 -0500

    add util to parse dates to settings module

--- VEIthim/R/LoadSettings.R
+++ VEIthim/R/LoadSettings.R
@@ -1,5 +1,4 @@
 # loads csv file containing ithim parameters
-
 loadSettings <- function(module_dir){
   
   # read settings
@@ -8,3 +7,17 @@ loadSettings <- function(module_dir){
   return(settings)
 }
   
+
+parseYears <- function(base_model, ref_year){
+  
+  # parse years from output dir
+  files <- Sys.glob(file.path(base_model, "results", "output", "*", "Household_*"))
+  getYear <- function(item){
+    as.integer(tail(strsplit(basename(file_path_sans_ext(item)), "_")[[1]], 1))
+  }
+  years <- lapply(files, getYear)
+  
+  # if there is a backcast year (ie, a year before the reference year) scrub it
+  years <- years[years >= ref_year]
+  return(years)
+}
