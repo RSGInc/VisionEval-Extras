3370a94b23a8ca00ce50635ceb7e4084ef4c707e
Theodore Mansfield <theodore.mansfield@rsginc.com>
2024-02-16 13:52:04 -0500

    docstrings

--- VEIthim/R/LoadSettings.R
+++ VEIthim/R/LoadSettings.R
@@ -1,3 +1,23 @@
+# this module contains helper funcs for settings/configuration management
+
+
+#' Load settings
+#'
+#' Helper function to load ithim settings
+#'
+#' This function performs the following steps:
+#'
+#' \itemize{
+#'  \item loads settings
+#'    }
+#'
+#' @param module_dir local ithim module directory
+#'
+#' @return data.table containing ithim settings
+#'
+#' @export
+
+
 # loads csv file containing ithim parameters
 loadSettings <- function(module_dir){
   
@@ -7,6 +27,30 @@ loadSettings <- function(module_dir){
   return(settings)
 }
   
+
+#' Parse year
+#'
+#' Helper function to extract list of years from VE output csv directory and 
+#' scrub years before the reference year (ie, backcasting years)
+#'
+#' This function performs the following steps:
+#'
+#' \itemize{
+#'  \item identifies all household files in a VE output csv directory
+#'  
+#'  \item parses year from each file in the list
+#'  
+#'  \item removes years that are before the reference year
+#'    }
+#'
+#' @param base_model local VE model directory
+#' @param ref_year reference base) year for ithim model
+#'
+#' @return list of years for which a VE model exists (excluding those before 
+#' the reference year)
+#'
+#' @export
+
 
 parseYears <- function(base_model, ref_year){
   
