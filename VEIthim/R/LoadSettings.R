6037cd781175f6f33a61da9a3f8f49882386cfaa
Theodore Mansfield <theodore.mansfield@rsginc.com>
2024-02-29 16:10:06 -0500

    consolidate settings, scrub unused libraries

--- VEIthim/R/LoadSettings.R
+++ VEIthim/R/LoadSettings.R
@@ -19,12 +19,54 @@
 
 
 # loads csv file containing ithim parameters
-loadSettings <- function(module_dir){
+loadSettings <- function(input_dir){
   
   # read settings
-  settings <- fread(file.path(module_dir, "settings.csv"))
+  settings <- fread(file.path(input_dir, "settings.csv"))
   
-  return(settings)
+  # and config settings
+  config <- fread(file.path(input_dir, "config.csv"))
+  
+  # move a couple items up a level
+  base_model <- config[name=="base_model"]$value
+  reference_model <- config[name=="reference_model"]$value
+  base_year <- as.numeric(config[name=="base_year"]$value)
+  
+  # now scrub from config
+  scrub <- c("base_model", "reference_model", "base_year")
+  config <- config[!name %in% scrub]
+
+  # let's not deal with .cnf files for now
+  # just parse scenarios from results
+  model_name <- config[name=="model_name"]$value
+  scenarios <- list.dirs(
+    file.path("models", model_name, "scenarios"),
+    recursive = F,
+    full.names = F)
+  
+  results_dir <- list.dirs(
+    file.path("models", model_name, "results", "output"),
+    recursive = F)
+  
+  # finally, identify future years from results files
+  # this assumes that each future year has a reference model
+  future_years <- basename(
+    Sys.glob(
+      file.path(results_dir, paste0("Household_", reference_model, "*"))
+    )
+  )
+  
+  # parse year(s)
+  future_years <- as.numeric(gsub("\\D", "", future_years))
+  
+  return(list(settings=settings,
+              config=config,
+              base_model=base_model,
+              base_year=base_year,
+              reference_model=reference_model,
+              future_years=future_years,
+              scenarios=scenarios,
+              results_dir=results_dir))
 }
   
 
